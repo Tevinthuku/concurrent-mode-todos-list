533e9041e13aecc1391a6467231923dcfaef9c4e
Tevinthuku <tevinthuku@gmail.com>
2021-07-20 15:20:12 +0300

    initial commit

---
+++ packages/server/server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+const PORT = process.env.PORT || 4000;
+const PRODUCTION = process.env.NODE_ENV === "production";
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello(name: String!): String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: ({ name }) => {
+    return `Hello there ${name}!`;
+  },
+};
+
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: !PRODUCTION,
+  })
+);
+app.listen(PORT);
