42b2e9b27a6dcf98419ae288508e6cb440766fbd
Tevinthuku <tevinthuku@gmail.com>
2021-07-22 14:40:29 +0300

    add anotations

--- packages/www/src/routes/Layout.js
+++ packages/www/src/routes/Layout.js
@@ -1,4 +1,8 @@
-import React from "react";
-export default function Layout(props) {
+// @flow
+import React, { type Node } from "react";
+type Props = {
+  children: Node,
+};
+export default function Layout(props: Props): React$Element<"div"> {
   return <div>{props.children}</div>;
 }
