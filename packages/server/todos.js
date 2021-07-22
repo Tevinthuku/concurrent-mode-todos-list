3555965cba21b5b87c0187b43c8fe34326907841
Tevinthuku <tevinthuku@gmail.com>
2021-07-22 08:00:03 +0300

    build basic todos API

---
+++ packages/server/todos.js
@@ -0,0 +1,13 @@
+const todosList = require("./MOCK_TODOS.json");
+
+class Todos {
+  list(count = 10) {
+    return todosList.slice(0, count);
+  }
+
+  get(id) {
+    return todosList.find((todo) => todo.id === id);
+  }
+}
+
+module.exports = Todos;
