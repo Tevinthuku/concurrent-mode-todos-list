9a202af4d2d4afac53c49236c2f9ef14dd232a99
Tevinthuku <tevinthuku@gmail.com>
2021-07-22 21:01:36 +0300

    hide query for the meantime

--- packages/www/src/routes/Todo.js
+++ packages/www/src/routes/Todo.js
@@ -6,17 +6,17 @@ import {
   type GraphQLTaggedNode,
 } from "react-relay/hooks";
 
-const TodoQuery: GraphQLTaggedNode = graphql`
-  query TodoQuery($id: ID!) {
-    todo(id: $id) {
-      text
-      id
-    }
-  }
-`;
+// const TodoQuery: GraphQLTaggedNode = graphql`
+//   query TodoQuery($id: ID!) {
+//     todo(id: $id) {
+//       text
+//       id
+//     }
+//   }
+// `;
 
 export default function Todo(props) {
-  const data = usePreloadedQuery(TodoQuery, props.prepared.todoDetailsQuery);
+  // const data = usePreloadedQuery(TodoQuery, props.prepared.todoDetailsQuery);
   return (
     <div>
       <h1>Todo Details</h1>
